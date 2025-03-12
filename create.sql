4b172522880fb1afb175ad1521453af69650bb49
cgui00 <c.gui080408@gmail.com>
2025-03-12 14:43:04 -0300

    ATV

---
+++ create.sql
@@ -0,0 +1,30 @@
+create database atividade
+default character set utf8mb4
+default collate utf8mb4_general_ci;
+
+create table clients (
+id_cliente int primary key not null unique auto_increment,
+nome varchar(100),
+gmail varchar(50) unique
+); 
+
+create table products (
+id_produto int primary key not null unique auto_increment,
+nomes varchar(30),
+princes varchar(10)
+);
+
+
+create table orders (
+id_orders int primary key not null unique auto_increment,
+tld int,
+cld int,
+dates date,
+statu varchar(30)
+);
+
+create table productsche (
+sId int,
+pId int,
+quantitys int
+);
---
+++ population.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO clients(nome, princs) VALUES ("Laurindo","email@email");
+INSERT INTO clients(nome, princs) VALUES ("Claudia","email1@email");
+INSERT INTO clients(nome, princs) VALUES ("Laura","email2@email");
+
+INSERT INTO products(nomes, princes) VALUES ("Ovo Frito","4.0");
+INSERT INTO products(nomes, princes) VALUES ("Hamburger","10.0");
+INSERT INTO products(nomes, princes) VALUES ("Picanha","200");
+INSERT INTO products(nomes, princes) VALUES ("Café","45");
+
+INSERT INTO orders(tld, cld, dates, statu) VALUES ("1","2","2025-03-29","'open'");
+INSERT INTO orders(tld, cld, dates, statu) VALUES ("3","1","2025-03-01","'open'");
+
+INSERT INTO productsche(sId, pId, quantitys) VALUES ("1","3","1");
+INSERT INTO productsche(sId, pId, quantitys) VALUES ("1","2","2");
+INSERT INTO productsche(sId, pId, quantitys) VALUES ("2","1","1");
