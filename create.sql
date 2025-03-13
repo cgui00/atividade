35f2e7ee2372a450a4a883b6b9d58425e7427a27
cgui00 <c.gui080408@gmail.com>
2025-03-13 16:26:29 -0300

    atv

--- create.sql
+++ create.sql
@@ -6,14 +6,14 @@ create table clients (
 id_cliente int primary key not null unique auto_increment,
 nome varchar(100),
 gmail varchar(50) unique
-); 
+);  
 
 create table products (
 id_produto int primary key not null unique auto_increment,
 nomes varchar(30),
 princes varchar(10)
 );
-
+    
 
 create table orders (
 id_orders int primary key not null unique auto_increment,
